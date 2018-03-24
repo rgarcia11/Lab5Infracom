4d65fc448661e9ccc28fd4cd38a9358d7d9710c9
AlejandroEcheverri <a.echeverrir@uniandes.edu.co>
2018-03-24 17:24:49 -0500

    hashing mods

--- src/cliente_udp_archivos.py
+++ src/cliente_udp_archivos.py
@@ -13,6 +13,7 @@ from ObjetoEnviar import *
 
 
 #Definicion de ip y puerto del servidor, socket para establecer comunicacion y direccion del servidor.
+
 IP = "127.0.0.1" 
 #input('Inserte la IP a la que desea conectarse: ')
 
@@ -24,7 +25,7 @@ nombre_archivo = "test.pdf"
 
 servidor = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 dir_servidor = (IP, PORT)
-TAM_BUFFER = 4096
+TAM_BUFFER = 1048576
 ACK = 'ACK'
 NAC = 'NAC'
 def pedirArchivo(nombre_archivo):
@@ -46,32 +47,65 @@ def pedirArchivo(nombre_archivo):
 	
 	#envia al servidor nombre del archivo a descargar
 	servidor.sendto(nombre_archivo.encode(), dir_servidor)
+	
+	try:
+		contador = 0
+		while True:
+			servidor.settimeout(3)
+			data,addr = servidor.recvfrom(TAM_BUFFER)
+			data = pickle.loads(data)
+			#print("data: "+ str(data[0]) + " hash: " + data[1])
+			hash_object = hashlib.md5(data[0])
+			if hash_object.hexdigest() == data[1]:
+				servidor.sendto(ACK.encode(), dir_servidor)
+				f.write(data[0])
+				print("se recibio bien paquete: " + str(contador))
+				contador = contador + 1
+			else:
+				servidor.sendto(NAC.encode(), dir_servidor)
+				print("llego corrupto el paquete: " + str(contador) )
+	except Exception as inst:
+		print(type(inst))
+		print(inst.args)
+		print(inst)
+		print("fin de descripcion de la excepcion")
+		f.close()
+		servidor.close()
+		print('Termino de transferir')
+	
+	
+	"""
 	fallo = 0
 	try:
 		servidor.settimeout(5)
 		data,addr = servidor.recvfrom(TAM_BUFFER)
 		data = pickle.loads(data)
-		servidor.sendto(ACK.encode(), dir_servidor)
+			servidor.sendto(ACK.encode(), dir_servidor)
 	except:
 		fallo = 1
 		print('El servidor no respondio.')
 
 	if not fallo:
 		try:
+			contador = 0
 			while True:
 				f.write(data[0])
 				servidor.settimeout(3)
 				data,addr = servidor.recvfrom(TAM_BUFFER)
 				data = pickle.loads(data)
+				print("data: "+ str(data[0]) + " hash: " + data[1])
 				hash_object = hashlib.md5(data[0])
 				if hash_object.hexdigest() == data[1]:
 					servidor.sendto(ACK.encode(), dir_servidor)
+					#print("se recibio bien paquete: " + str(contador))
+					#contador = contador + 1
 				else:
 					servidor.sendto(NAC.encode(), dir_servidor)
+					#print("llego corrupto el paquete: " + str(contador) )
 		except:
 			f.close()
 			servidor.close()
 			print('Termino de transferir')
-
+	"""
 if __name__ == "__main__":
 	pedirArchivo(nombre_archivo)
--- src/servidor_udp_archivos.py
+++ src/servidor_udp_archivos.py
@@ -21,10 +21,11 @@ IP = "127.0.0.1"
 PORT = 8081
 #int(input('Inserte el puerto en el que desea escuchar conexiones: '))
 
+#Params iniciales
 dir_servidor = (IP, PORT)
 socketServidor = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 socketServidor.bind(dir_servidor)
-TAM_BUFFER = 4096
+TAM_BUFFER = 1048576
 TAM_MSG = 1024
 dir_src = os.getcwd()
 dir_data = os.path.join(dir_src[:-(len(os.sep)+len("src"))],"data")
@@ -42,10 +43,13 @@ def enviarObjetos():
 			Se utilizara addr para responderle a ese cliente.
 	"""
 	
-	#recibe el nombre del archivo
+	#recibe el nombre del archivo y lo pasa devuelta a string
 	data, addr = socketServidor.recvfrom(TAM_BUFFER)
-	nombre_archivo = data.strip()
+	nombre_archivob = data.strip()
+	nombre_archivo = nombre_archivob.decode()
 	
+	print(nombre_archivo)
+
 	#dir, ip y puerto
 	dir_cliente = addr
 	
@@ -53,56 +57,43 @@ def enviarObjetos():
 	f=open(nombre_archivo, 'rb')
 	socketCliente = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 	
-	while data:
-		#dataEnv contiene en [0] el mensaje y en [1] el mensaje hasheado
-		dataEnv = []
-		dataEnv.append(f.read(TAM_MSG))
-		hash_object = hashlib.md5(dataEnv[0])
-		dataEnv.append(hash_object.hexdigest())
-		print(str(dataEnv[1]))
-		if socketCliente.sendto(pickle.dumps(dataEnv),addr):
-			print('Se esta enviando 1 pedacito')
+	#  -- algunas variables ---
+	condicionG = True # dice si acabo de enviar
+	contador = 0 # cuenta la cantidad de paquetes enviados con respuesta de que llegaron bien
+	leerNuevo = True # dice si hay que leer mas de el archivo para reenviar (TRUE) o si es necesario reenviar (FALSE)
+	dataEnv = [] #dataEnv contiene en [0] el mensaje en bytes y en [1] el mensaje hasheado como un string
+	#  -------
+
+	while condicionG: # si no hay nada que enviar termina
+		if leerNuevo: 
+			# solo se cambian los datos a enviar 
+			dataEnv.append(f.read(TAM_MSG)) #Guarda la porcion del archivo (datos) a mandar en el paquete 
+			if dataEnv[0]:
+				#Guarda el hash del mensaje para verificacion de integridad
+				hash_object = hashlib.md5(dataEnv[0])
+				dataEnv.append(hash_object.hexdigest())
+				print("data:"+ str(dataEnv[0]) + " hash: " + dataEnv[1])
+			else:
+				condicionG = False
+		if condicionG: 
+			#envia dataEnv con pickle al addr(puerto & ip)
+			socketCliente.sendto(pickle.dumps(dataEnv), addr)
+			print('Se envio 1 paquete')
 			try:
-				socketServidor.settimeout(3)
+				socketServidor.settimeout(2)
 				resp, addr = socketServidor.recvfrom(TAM_BUFFER)
 				if resp == b'ACK':
-					data = f.read(TAM_BUFFER)
+					contador = contador + 1
+					leerNuevo = True
+					print('Se recibio respuesta ACK ! bien')
+				else:
+					leerNuevo = False #El paquete le llego corrupto
+					print('Se recibio respuesta NAC')
 			except Exception:
-				pass
-
+				print('No se recibio ACK')
+				leerNuevo = False  #El paquete no llego, se perdio la respuesta o el cliente no esta conectado
 	socketCliente.close()
 	f.close()
-
-
-def verificarTimeout():
-	"""
-	Verifica si hay una "sesion" con un cliente (ip) que expiro.
-	El tiempo de expiracion (timeout) es de 5 segundos.
-	Cada dos segundos se verifica el timeout de todos los clientes.
-	"""
-	while True:
-		time.sleep(2)
-		print('Clientes enviando:{}'.format(tiempos))
-		tiempo_comparacion = time.time()
-		with lock:
-			for ip in list(tiempos):
-				tiempo_comparacion = abs(tiempo_comparacion - tiempos[ip][0])
-				if tiempo_comparacion > 5:
-					#Se deben imprimir las estadisticas en el archivo:
-					num_recibidos = tiempos[ip][1]
-					num_esperados = tiempos[ip][2]
-					num_perdidos = num_esperados - num_recibidos
-					nombretxt = str(ip) + ".txt"
-					promedio = (tiempos[ip][3])/num_esperados
-					os.chdir(dir_data)
-					file = open(nombretxt,"a")
-					file.write('Transferencia terminada. Recibidos: {}. Total esperados: {}. Perdidos: {}. Promedio tiempo envio: {}\n'.format(num_recibidos, num_esperados, num_perdidos, promedio))
-					file.close()
-					os.chdir(dir_src)
-					del tiempos[ip]
-
-
-
 
 if __name__ == "__main__":
 	lock = threading.RLock()
@@ -110,4 +101,6 @@ if __name__ == "__main__":
 		target = enviarObjetos
 	)
 	thread_enviarObjetos.start()
-	#verificarTimeout()
+	
+	
+#C:\Users\ale_e\Desktop\Alejandro\UNIVERSIDAD ANDES\6to Semestre (Local)\Infraestructura de Comunicaciones\Lab5\Lab5Infracom\src
