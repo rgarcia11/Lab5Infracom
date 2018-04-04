81c3ea9cb0814f7c8592bf5cda88a1064298c9e3
Rogelio Garcia Escallon <r.garcia11@sis.virtual.uniandes.edu.co>
2018-04-04 09:17:17 -0500

    Documentacion y orden

--- src/cliente_tcp.py
+++ src/cliente_tcp.py
@@ -4,10 +4,9 @@ import tkinter
 import time
 import os
 import interfaz_cliente_tcp
-############################################################
-#             COMUNICACION CON EL SERVIDOR!
-############################################################
-
+"""
+Script de conexion con el servidor y transferencia de archivos
+"""
 
 TAM_BUFFER = 5120
 lista_archivos = []
@@ -16,11 +15,11 @@ inicio_descarga = 0
 dir_src = os.getcwd()
 dir_descargas = os.path.join(dir_src[:-(len(os.sep)+len("src"))],"descargas")
 cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#nombre_servidor = '52.234.215.61'
-#nombre_servidor = '127.0.0.1'
 
-#Maneja la conexion con el servidor
 def conexion_con_servidor(interfaz, nombre_servidor0, puerto_servidor0):
+	"""
+	Maneja la conexion con el servidor. Lanzara error si no lo logra.
+	"""
     global lista_archivos
     global estado_conexion
     global inicio_descarga
@@ -30,8 +29,6 @@ def conexion_con_servidor(interfaz, nombre_servidor0, puerto_servidor0):
     nombre_servidor = nombre_servidor0
     puerto_servidor = puerto_servidor0
 
-
-    #Termina la conexion
     print('Intentare conectarme a {}:{}'.format(nombre_servidor, puerto_servidor))
     cliente.connect((nombre_servidor, puerto_servidor))
     print('conectado')
@@ -45,29 +42,12 @@ def conexion_con_servidor(interfaz, nombre_servidor0, puerto_servidor0):
 
     lista_archivos = lista_archivos[3:-2].split(", ")
     interfaz.actualizarLista(lista_archivos)
-    #print(a for a in lista_archivos)
-    #for a in lista_archivos:
-        #print(a)
-    #print(type(lista_archivos))
-    #print(lista_archivos)
 
-    #Se inicia el proceso de timeout para que no demore mas de 15 s en escoger
-    #thread_timeout=threading.Thread(
-    #    target=timeout_cliente
-    #)
-    #thread_timeout.start()
 
-    #Se inicia el proceso de pedir un archivo!
-    #thread_archivo=threading.Thread(
-    #    target=pedir_archivo
-    #)
-    #thread_archivo.start()
-
-#peticion del archivo
 def pedir_archivo(interfaz, mensaje):
-    #mensaje = input('Ingrese el nombre del archivo a descargar, o deje vacio para terminar: ')
-    #Este while es para tener comunicacion mientras no escriba vacio
-
+	"""
+	Se encarga de pedir un archivo y descargarlo.
+	"""
     print('Pedi {}'.format(mensaje))
     cliente.sendto(mensaje.encode(),(nombre_servidor, puerto_servidor))
     print('Mensaje enviado')
@@ -84,18 +64,12 @@ def pedir_archivo(interfaz, mensaje):
         os.chdir(dir_descargas)
         with open(mensaje, 'wb') as f:
             while tam_actual < tam_archivo:
-                #print('Tamanho actual del archivo: {}'.format(tam_actual))
-                #print('Tamanho del archivo: {}'.format(tam_archivo))
-                #if not interfaz.estaConectado():
-                    #print('debo moril')
                 if not interfaz.estaConectado():
                     cliente.close()
                     return
 
                 if interfaz.estaDescargando():
                     progreso = tam_actual/tam_archivo*100
-
-                    #print('Recibiendo... {0:.1f}%'.format(progreso))
 
                     archivo_recibir = cliente.recv(TAM_BUFFER)
                     num_archivos+=1
@@ -105,13 +79,11 @@ def pedir_archivo(interfaz, mensaje):
                         archivo_recibir = archivo_recibir[:tam_archivo-tam_actual]
                     buff += archivo_recibir
                     tam_actual += len(archivo_recibir)
-                    #print('chunk: {}'.format(archivo_recibir))
                     f.write(archivo_recibir)
                     interfaz.actualizarProgreso(progreso)
         tiempo_final = time.time()
         cliente.sendto(str(tiempo_final).encode(),(nombre_servidor, puerto_servidor))
         tiempo_transcurrido = str(cliente.recv(TAM_BUFFER).decode())
-        #print('Archivo: {}'.format(archivo_recibir))
         tam_diferencia = tam_archivo - tam_actual
         if tam_diferencia == 0:
             print('Recibido archivo completo. Tiempo transcurrido: {}. Bytes esperados: {}. Bytes recibidos: {}. Paquetes recibidos: {}'.format(tiempo_transcurrido,tam_archivo,tam_actual,num_archivos))
@@ -122,6 +94,9 @@ def pedir_archivo(interfaz, mensaje):
         cliente.close()
 
 if __name__ == '__main__':
+	"""
+	Si se corre el programa, las interacciones son por consola.
+	"""
     print('inicio!')
     nombre_servidor = input('Digite IP del servidor (Azure: 52.234.215.61. Local: 127.0.0.1): ')
     puerto_servidor = int(input('Digite puerto del servidor (5005): '))
--- src/interfaz_cliente_tcp.py
+++ src/interfaz_cliente_tcp.py
@@ -2,17 +2,25 @@ import tkinter as tk
 from cliente_tcp import *
 import threading
 import math
+"""
+Interfaz del cliente para manejar las interacciones del usuario.
+"""
 class Application(tk.Frame):
+	"""
+	Esta clase genera una interfaz
+	"""
     def __init__(self, master=None):
+		"""
+		Constructor
+		"""
         super().__init__(master)
         self.pack()
         self.create_widgets()
 
     def create_widgets(self):
-        #self.hi_there = tk.Button(self)
-        #self.hi_there["text"] = "Hello World\n(click me)"
-        #self.hi_there["command"] = self.say_hi
-        #self.hi_there.pack(side="top")
+		"""
+		Crea todos los elementos de la ventana
+		"""
         self.descargando = 0
         self.conexion = 0
 
@@ -56,24 +64,36 @@ class Application(tk.Frame):
         self.quit.grid(row=4, column=2)
 
     def conectar(self):
+		"""
+		Llama la funcion conectar del cliente. Inicia el estado de la conexion.
+		"""
         conexion_con_servidor(self, self.txip.get(), int(self.txport.get()))
         self.mostrarConexion()
         thread_timeout=threading.Thread(target=self.timeoutCliente)
         thread_timeout.start()
 
     def actualizarLista(self, lista):
+		"""
+		Actualiza la lista de archivos.
+		"""
         self.lista = lista
         self.listBoxLista.delete(0,tk.END)
         for l in self.lista:
             self.listBoxLista.insert(tk.END, l[1:-1])
 
     def descargarArchivo(self):
+		"""
+		Llama la funcion descargar del cliente. Inicia el estado "descargando"
+		"""
         seleccionado = self.listBoxLista.get(self.listBoxLista.curselection())
         thread_archivo=threading.Thread(target=pedir_archivo, args=(self,seleccionado,))
         thread_archivo.start()
         self.detenerDescarga()
 
     def mostrarConexion(self):
+		"""
+		Cambia el estado de la conexion y lo muestra.
+		"""
         if self.conexion:
             self.conexion = 0
             self.sconexion.set("Desconectado")
@@ -82,12 +102,21 @@ class Application(tk.Frame):
             self.sconexion.set("Conectado")
 
     def estaConectado(self):
+		"""
+		Retorna el estado de la conexion.
+		"""
         return self.conexion
 
     def actualizarProgreso(self,progreso):
+		"""
+		Actualiza el porcentaje de progreso de la descarga.
+		"""
         self.s.set('Progreso: {}%'.format(math.ceil(progreso)))
 
     def detenerDescarga(self):
+		"""
+		Detiene la descarga cambiando el estado y mostrandolo.
+		"""
         if self.descargando:
             self.sdetener.set("Reanudar")
             self.descargando=0
@@ -96,9 +125,15 @@ class Application(tk.Frame):
             self.descargando=1
 
     def estaDescargando(self):
+		"""
+		Retorna si se esta descargando actualmente o no.
+		"""
         return self.descargando
 
     def timeoutCliente(self):
+		"""
+		Calcula cuando se debe terminar la sesion.
+		"""
         while 1:
             time.sleep(5)
             if not self.descargando:
@@ -107,6 +142,9 @@ class Application(tk.Frame):
                 return
 
 if __name__ == '__main__':
+	"""
+	Si se llama este metodo, se crea la interfaz con la clase Application y se inicia
+	"""
     root = tk.Tk()
     app = Application(master=root)
     app.mainloop()
